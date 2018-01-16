47b7767ba6ee906ac02bac8971d27ac33a379055
Stefan Schindler <ss@limbozz.com>
2018-01-16 15:37:04 +0100

    API: Added journal_entries endpoint.

--- src/graphql_api/types.py
+++ src/graphql_api/types.py
@@ -1,5 +1,18 @@
 import graphene
 
+class OwnerJournalEntryType(graphene.ObjectType):
+  description = graphene.String()
+  creation_time = graphene.types.datetime.DateTime(required=True)
+  booking_time = graphene.types.datetime.DateTime(required=True)
+  valuta_time = graphene.types.datetime.DateTime(required=True)
+  value = graphene.Int(required=True)
+  #currency
+
 class MeType(graphene.ObjectType):
-  username = graphene.String()
-  email = graphene.String()
+  username = graphene.String(required=True)
+  email = graphene.String(required=True)
+
+  journal_entries = graphene.List(OwnerJournalEntryType)
+
+  def resolve_journal_entries(user, info):
+    return user.created_journal_entries.all()
