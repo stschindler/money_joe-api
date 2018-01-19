6884bd0f334e71d95ddf28114f5381aa529fa070
Stefan Schindler <ss@limbozz.com>
2018-01-19 17:38:24 +0100

    Moved everything to schema file. Implemented Household Relay node. Not nice :(

--- src/graphql_api/schema.py
+++ src/graphql_api/schema.py
@@ -1,10 +1,44 @@
-from . import types
+from . import decorators
+from household.helpers import find_user_households
+from journal.helpers import find_household_entries_for_user
 
 import graphene
 
+class Household(graphene.ObjectType):
+  class Meta:
+    interfaces = (graphene.relay.Node,)
+
+  name = graphene.String(required=True)
+
+  @classmethod
+  def get_node(cls, info, id):
+    result = None
+
+    if info.context.user.is_authenticated is True:
+      household = find_user_households(info.context.user) \
+        .filter(id=id) \
+        .first()
+
+      if household is not None:
+        result = Household(id=household.id, name=household.name)
+
+    return result
+
+class HouseholdConnection(graphene.relay.Connection):
+  class Meta:
+    node = Household
+
+class Me(graphene.ObjectType):
+  class Meta:
+    interfaces = (graphene.relay.Node,)
+
+  username = graphene.String(required=True)
+  email = graphene.String(required=True)
+
 class Query(graphene.ObjectType):
   node = graphene.relay.Node.Field()
-  me = graphene.Field(types.Me)
+  me = graphene.Field(Me)
+  households = graphene.relay.ConnectionField(HouseholdConnection)
 
   def resolve_me(instance, info):
     return (
@@ -12,5 +46,13 @@ class Query(graphene.ObjectType):
       if info.context.user.is_authenticated is True
       else None
     )
+
+  def resolve_households(instance, info):
+    households = []
+
+    if info.context.user.is_authenticated is True:
+      households = find_user_households(info.context.user)
+
+    return households
 
 schema = graphene.Schema(query=Query)
